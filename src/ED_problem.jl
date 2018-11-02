09c9c70d52213df4664059efc82615f9aa8a0a76
Jose Daniel Lara <jdlara@berkeley.edu>
2018-11-01 18:53:55 -0700

    Include the ED Problem

--- src/ED_problem.jl
+++ src/ED_problem.jl
@@ -1,3 +1,11 @@
-base_dir = dirname(dirname(pathof(PowerSystems)))
-include(joinpath(base_dir,"data/data_5bus.jl"))
-sys = PowerSystem(nodes5, generators5, loads5_DA, branches5, nothing,  1000.0);
+function solve_ed(sys, optimizer)
+    Net = PS.CopperPlatePowerModel
+    m = Model(optimizer);
+    netinjection = PS.instantiate_network(Net, sys);
+    PS.constructdevice!(m, netinjection, ThermalGen, PS.ThermalDispatch, Net, sys)
+    PS.constructdevice!(m, netinjection, RenewableGen, PS.RenewableCurtail, Net, sys)
+    PS.constructnetwork!(m, [(device=Line, formulation=PS.PiLine)], netinjection, Net, sys)
+    @objective(m, Min, m.obj_dict[:objective_function])
+    JuMP.optimize!(m)
+    return get_model_result(m)
+end
